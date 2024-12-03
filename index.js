9b46f18882a43947f6f98059c5a65e404e2ed1e4
Tharindu37 <tharindut520@gmail.com>
2024-12-04 01:47:56 +0530

    setup eureka client

--- index.js
+++ index.js
@@ -3,12 +3,39 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const OrderRoute = require('./routes/OrderRoute');
+const {Eureka} = require('eureka-js-client')
 
 // Initialize dotenv to load environment variables
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const EUREKA_SERVER = 'localhost'
+
+const client = new Eureka({
+    instance: {
+        app: 'ORDER-PAYMENT-API',
+        instanceId: 'order-payment-api',
+        hostName: 'localhost',
+        ipAddr: '127.0.0.1',
+        statusPageUrl: `http://localhost:${PORT}`,
+        port: {
+            $: PORT,
+            '@enabled': true
+        },
+        vipAddress: 'order-payment-api',
+        dataCenterInfo: {
+            '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+            name: 'MyOwn' // Indicates it's not AWS
+        }
+    },
+    eureka: {
+        host: EUREKA_SERVER,
+        servicePath: '/eureka/apps/',
+        port: 8761
+    }
+});
+
 
 // Middleware for parsing JSON and URL-encoded data
 app.use(bodyParser.json());
@@ -35,6 +62,14 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send({ success: false, message: 'Something went wrong!', error: err.message });
 });
+
+client.start((error)=>{
+    if(error){
+        console.log('error', error)
+    }else{
+        console.log('success!')
+    }
+})
 
 // Start the server
 app.listen(PORT, () => {
